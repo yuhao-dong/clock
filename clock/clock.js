83d66d5c05ef23fc8c6051b5bc7cb7eb565884ec
yuhao-dong <73535417+yuhao-dong@users.noreply.github.com>
2022-06-17 18:42:47 -0400

    Update clock.js

--- clock/clock.js
+++ clock/clock.js
@@ -1,1 +1,19 @@
-let 
+let hr = document.querySelector("#hr");
+let mn = document.querySelector("#mn");
+let sc = document.querySelector("#sc");
+
+// calls a function at specified intervals
+setInterval(() => {
+    let day = new Date();
+    // make max hh mm ss = 360 degrees for rotation
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * 6;
+    let ss = day.getSeconds() * 6;
+    
+    console.log(day + " -- " + hh + " -- " + mm + " -- " + ss);
+    
+    hr.style.transform = `rotateZ(${hh + (mm / 12)}deg)`;
+    mn.style.transform = `rotateZ(${mm}deg)`;
+    sc.style.transform = `rotateZ(${ss}deg)`;
+})
+
